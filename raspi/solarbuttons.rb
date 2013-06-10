4a4f48fd02da96fa0108922b9c47d682bd00f801
Till Mossakowski <till@communtu.de>
2013-06-10 20:35:04 +0200

    corrected coordinates

--- raspi/solarbuttons.rb
+++ raspi/solarbuttons.rb
@@ -116,8 +116,8 @@ include SolarControl
     puts "time_selection"
     clear_screen
     print_xy(0,4, "Dauer des Programms:")
-    print_xy(10,4, "#{session[:duration][0]}:#{session[:duration][1]}")
-    print_xy(20,4, "Wann soll die Maschine spätestens fertig sein?")
+    print_xy(0,14, "#{session[:duration][0]}:#{session[:duration][1]}")
+    print_xy(0,24, "Wann soll die Maschine spätestens fertig sein?")
     d = if session[:day] == 0 then "Heute" else "Morgen" end
     s = ""  
     (0..session[:time].length-1).each do |digit| 
@@ -132,7 +132,7 @@ include SolarControl
         s << "]" 
       end 
     end 
-    print_xy(30,4, "#{d} um #{s} Uhr")
+    print_xy(0,34, "#{d} um #{s} Uhr")
     write_framebuffer
   end
   
